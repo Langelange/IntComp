dcb280cfe278d81ea478d0c9aa60e47a584e669c
Langelange <jpflange@gmail.com>
2021-11-13 18:11:52 -0300

    Adicionada atividade de nome e data e operações aritméticas

---
+++ Matematica.py
@@ -0,0 +1,21 @@
+import math
+
+num1 = int(input('Digite um número: '))
+antecessor = num1 - 1
+sucessor = num1 + 1
+
+print('O antecessor e o sucessor de', num1, 'é:', antecessor, 'e', sucessor,'.')
+print('Seu dobro é:', num1*2)
+print('Seu cubo é:', num1**3)
+print('Sua raíz é:', num1**(0.5))
+print('Tabuada: ',
+num1*1,
+num1*2,
+num1*3,
+num1*4,
+num1*5,
+num1*6,
+num1*7,
+num1*8,
+num1*9,
+)
---
+++ NomeData.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu nome: ')
+dia = input('Informe o dia de nascimento (xx): ')
+mes = input('Informe o mês de nascimento: ')
+ano = input('Informe o ano de nascimento (xxxx): ')
+
+print('Seja bem vindo(a),', nome,'!')
+
+print('Sua data de nascimento é:',dia, 'de', mes, 'de', ano)
